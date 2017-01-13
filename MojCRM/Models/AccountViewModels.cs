6598bea4e3fbad2575c77991b2e4b99964c3cf2e
Alen David Jeđud <alen.jedud@hotmail.com>
2017-01-13 20:58:01 +0100

    Update osnovnih postavki

--- MojCRM/Models/AccountViewModels.cs
+++ MojCRM/Models/AccountViewModels.cs
@@ -60,10 +60,10 @@ namespace MojCRM.Models
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lozinka")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Zapamti me?")]
         public bool RememberMe { get; set; }
     }
 
