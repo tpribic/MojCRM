221528c93c61a416b93106536e17d9dfa3f5ae15
Alen David Jeđud <alen.jedud@hotmail.com>
2017-10-06 20:20:45 +0200

    Dorađeno: - mali refactoring Organizations modela - dopuna inicijalnog pregleda tvrtki - ispravak layouta kod registracije novog korisnika

--- MojCRM/Models/Organizations.cs
+++ MojCRM/Models/Organizations.cs
@@ -5,10 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
 
 namespace MojCRM.Models
 {
